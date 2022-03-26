1cc4eb80843a37a0cc3e21bfff7a7c0be29f457c
Sebastian Posada Marín <sebasmarin1099@gmail.com>
2022-03-26 16:06:01 -0500

    States

--- Shopping/Shopping/Controllers/CountriesController.cs
+++ Shopping/Shopping/Controllers/CountriesController.cs
@@ -40,7 +40,24 @@ namespace Shopping.Controllers
 
             return View(country);
         }
-        
+        public async Task<IActionResult> DetailsState(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            State state = await _context.States
+                .Include(s => s.Cities)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return View(state);
+        }
+
         public IActionResult Create()
         {
             Country country = new() { States = new List<State>() };
@@ -141,7 +158,9 @@ namespace Shopping.Controllers
                 return NotFound();
             }
 
-            Country country = await _context.Countries.FindAsync(id);
+            Country country = await _context.Countries
+                .Include(c => c.States)
+                .FirstOrDefaultAsync(c => c.Id == id);
             if (country == null)
             {
                 return NotFound();
